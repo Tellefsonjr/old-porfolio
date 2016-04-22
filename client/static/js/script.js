9c49cf60351232bf43b15255f281d79d5271d522
Tellefsonjr <Timmeyellefson@gmail.com>
2016-04-21 22:04:21 -0700

    added .php trick

--- client/static/js/script.js
+++ client/static/js/script.js
@@ -1,16 +1,3 @@
-$(function() {
-	$.scrollify({
-		section:".panel"
-	});
-
-	$(".scroll,.scroll-btn").click(function(e) {
-		e.preventDefault();
-
-		$.scrollify.next();
-	});
-
-
-});
 $("#dojo_btn").click(function() {
     $('html,body').animate({
         scrollTop: $("#dojo_gram_feature").offset().top},
